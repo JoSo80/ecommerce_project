f42883bbbafa569e20ecdac8a05ac7189da1903a
JoSo80 <josrobsor@gmail.com>
2020-11-24 08:59:15 -0600

    added active admin and device

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/show'
-  get 'products/index'
-  get 'products/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
+  resources :categories, only: %i[index show]
+  resources :products, only: :show do
+    collection do
+      get :search
+    end
+  end
+
+  root to: "categories#index"
 end
