27fc5a49c3877f90acf201df94f5453bfb72b304
JoSo80 <josrobsor@gmail.com>
2020-11-26 16:18:28 -0600

    set up models and updated seed script

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_212237) do
+ActiveRecord::Schema.define(version: 2020_11_26_221014) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -20,12 +20,12 @@ ActiveRecord::Schema.define(version: 2020_11_26_212237) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
-    t.string "type"
     t.string "color"
     t.integer "price"
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "set"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+NUMBER_OF_CATEGORIES = 4
+PRODUCTS_PER_CATEGORY = 4
+
+Product.delete_all
+Category.delete_all
+
+NUMBER_OF_CATEGORIES.times do
+  category = Category.create(name: Faker::Ancient.unique.god)
+
+  PRODUCTS_PER_CATEGORY.times do
+    product = category.products.create(
+      name:  Faker::Ancient.primordial,
+      set:   Faker::Ancient.titan,
+      color: Faker::Color.hex_color,
+      price: rand(1..15)
+    )
+  end
+end
+
+puts "Created #{Category.count} Categories."
+puts "Created #{Product.count} Products."
