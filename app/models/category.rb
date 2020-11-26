1d2a213df94b5b69a8676aec6f26b557b2b41779
JoSo80 <josrobsor@gmail.com>
2020-11-26 13:34:18 -0600

    added validation to category and product models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
   has_many :products
+  validates :set_name, presence: true
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :category
+  validates :name, :type, :color, :description, :price, presence: true
 end
