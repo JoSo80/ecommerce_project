03310dc6cf8c5cbcd4c9d49dac820d2b9c909c03
JoSo80 <josrobsor@gmail.com>
2020-11-27 11:39:30 -0600

    updated schema and seed script with pages info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_013855) do
+ActiveRecord::Schema.define(version: 2020_11_27_172616) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
@@ -61,6 +61,14 @@ ActiveRecord::Schema.define(version: 2020_11_27_013855) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.string "permalink"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,11 +3,24 @@ PRODUCTS_PER_CATEGORY = 25
 
 Product.delete_all
 Category.delete_all
+Page.delete_all
+
+Page.create(
+  title:     "About Us",
+  content:   "This website contains our MTG card collection",
+  permalink: "about_us"
+)
+
+Page.create(
+  title:     "Contact Us",
+  content:   "If you want contact us please email themagicexchange@gmail.com",
+  permalink: "contact_us"
+)
 
 NUMBER_OF_CATEGORIES.times do
-  category = Category.create(name: Faker::Ancient.unique.god)
+category = Category.create(name: Faker::Ancient.unique.god)
 
-  PRODUCTS_PER_CATEGORY.times do
+PRODUCTS_PER_CATEGORY.times do
     product = category.products.create(
       name:  Faker::Ancient.primordial,
       set:   Faker::Ancient.titan,
