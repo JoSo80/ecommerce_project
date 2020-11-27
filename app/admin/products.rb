6336c0f25725d19b6ec9abc87297424fd9aedf51
JoSo80 <josrobsor@gmail.com>
2020-11-26 20:36:47 -0600

    updated admin params, prodoct show and controller

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Product do
-  permit_params :name, :color, :price, :category_id, :set
+  permit_params :name, :color, :price, :category_id, :set, :image
+
+  form do |f|
+    f.semantic_errors # shows errors on :base
+    f.inputs          # builds an input field for every attribute
+    f.inputs do
+      f.input :color, as: :string
+    end
+    f.inputs do
+      f.input :image, as: :file
+    end
+    f.actions         # adds the 'Submit' and 'Cancel' buttons
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,8 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def search
+    @products = Product.where("name LIKE ?", "%#{params[:search_term]}%")
+  end
 end
